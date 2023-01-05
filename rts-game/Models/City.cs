488821746f630561c6ebcfd98193c0de59f84cea
Richard Oden <richard.thomas.oden@gmail.com>
2023-01-05 06:56:58 -0600

    Created City class with a few basic properties.

---
+++ rts-game/Models/City.cs
@@ -0,0 +1,35 @@
+﻿using rts_game.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rts_game.Models
+{
+    internal class City : IMappable
+    {
+        private int _infrastructure;
+        private int _population;
+
+        public int MaxInfrastructure { get; set; }
+
+        public int Infrastructure
+        {
+            get => _infrastructure;
+            set => _infrastructure = Math.Min(MaxInfrastructure, value);
+        }
+
+        public int MaxPopulation { get; set; }
+
+        public int Population 
+        {
+            get => _population;
+            set => _population = Math.Min(MaxPopulation, value);
+        }
+
+        public int Radius => Population / 5;
+
+        public Coordinate Coordinate { get; set; }
+    }
+}
--- rts-game/Models/World.cs
+++ rts-game/Models/World.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using rts_game.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,6 @@ namespace rts_game.Models
 
         public int Height { get; set; }
 
-
+        public HashSet<IMappable> Mappables { get; set; } = new();
     }
 }
