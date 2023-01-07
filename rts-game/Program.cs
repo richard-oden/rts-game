936b72b2b0d6a91b8bff54ac0dda5179ed266069
Richard Oden <richard.thomas.oden@gmail.com>
2023-01-07 14:02:15 -0600

    Added logic to render outside border in Renderer.

--- rts-game/Program.cs
+++ rts-game/Program.cs
@@ -1,25 +1,16 @@
-﻿using static System.Console;
-
-namespace rts_game;
+﻿namespace rts_game;
 
 class Program
 {
     private static long _ticks = 0;
-    private static int _windowWidth = 120;
-    private static int _windowHeight = 30;
 
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Setup();
-        Run();
+        Renderer.Setup();
+        await Run();
     }
 
-    public static void Setup()
-    {
-        CursorVisible = false;
-        SetWindowSize(_windowWidth, _windowHeight);
-        SetBufferSize(_windowWidth, _windowHeight);
-    }
+
 
     public static async Task Run()
     {
--- rts-game/Renderer.cs
+++ rts-game/Renderer.cs
@@ -3,11 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace rts_game
 {
     public static class Renderer
     {
-        
+        private static int _windowWidth = 150;
+        private static int _windowHeight = 40;
+
+        public static void Setup()
+        {
+            CursorVisible = false;
+            SetWindowSize(_windowWidth, _windowHeight);
+            SetBufferSize(_windowWidth, _windowHeight);
+            DrawBorders();
+        }
+
+        public static void DrawBorders()
+        {
+            static void DrawHorizontalBorder(int y)
+            {
+                for (var x = 1; x < _windowWidth - 1; x++)
+                {
+                    SetCursorPosition(x, y);
+                    Write('═');
+                }
+            }
+
+            static void DrawVerticalBorder(int x)
+            {
+                for (var y = 1; y < _windowHeight - 1; y++)
+                {
+                    SetCursorPosition(x, y);
+                    Write('║');
+                }
+            }
+
+            DrawHorizontalBorder(0);
+            DrawHorizontalBorder(_windowHeight - 1);
+            DrawVerticalBorder(0);
+            DrawVerticalBorder(_windowWidth - 1);
+
+            SetCursorPosition(0, 0);
+            Write('╔');
+
+            SetCursorPosition(_windowWidth - 1, 0);
+            Write('╗');
+
+            SetCursorPosition(0, _windowHeight - 1);
+            Write('╚');
+
+            SetCursorPosition(_windowWidth - 1, _windowHeight - 1);
+            Write('╝');
+        }
     }
 }
